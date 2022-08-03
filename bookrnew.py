926410efa474f762c54ff6720931e418357301e9
Hayder Al-Chalabi <96077191+hay90der@users.noreply.github.com>
2022-08-04 02:02:45 +0300

    Update bookrnew.py

--- bookrnew.py
+++ bookrnew.py
@@ -42,5 +42,75 @@ to_read = pd.read_csv("to_read.csv")
 
 
 
+tf = TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0, stop_words='english')
+tfidf_matrix = tf.fit_transform(books['authors'])
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+books_with_tags = pd.merge(books, tags_join_DF, left_on='book_id', right_on='goodreads_book_id', how='inner')
+
+
+temp_df = books_with_tags.groupby('book_id')['tag_name'].apply(' '.join).reset_index()
+
+
+books = pd.merge(books, temp_df, left_on='book_id', right_on='book_id', how='inner')
+
+books['corpus'] = (pd.Series(books[['authors', 'tag_name']]
+                .fillna('')
+                .values.tolist()
+                ).str.join(' '))
+
+tf_corpus = TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0, stop_words='english')
+tfidf_matrix_corpus = tf_corpus.fit_transform(books['corpus'])
+cosine_sim_corpus = linear_kernel(tfidf_matrix_corpus, tfidf_matrix_corpus)
+
+# Build a 1-dimensional array with book titles
+titles = books['title']
+indices = pd.Series(books.index, index=books['title'])
+
+from colorama import Fore, Back, Style
+def printmd(string):
+    display(Markdown(string))
+
+# Function that get book recommendations based on the cosine similarity score of books tags
+def corpus_recommendations(title):
+    idx = indices[title]
+    sim_scores = list(enumerate(cosine_sim_corpus[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:11]
+    book_indices = [i[0] for i in sim_scores]
+    
+    # create an Empty DataFrame object
+    df = pd.DataFrame(titles.iloc[book_indices])
+    df['authors'] = books.loc[books.index.values,"authors"]
+    df['similarity_score'] = [row[1] for row in sim_scores]
+    
+    return df 
+    # return titles.iloc[book_indices]
+
+# corpus_recommendations("The Hobbit")
+
+defbook = 'The Hobbit'
+bookname = st.text_input('Enter your book name',defbook)
+#bookname = input()
+
+st.table(corpus_recommendations(bookname))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 st.markdown(""" Informatics Institute for Postgraduate Studies - IIPS """)
 st.markdown(""" Developed By : Hayfaa Khudir """)
