8a5c83f85ec5d1a5cb0c266fcd8e9720a3879aa6
Hayder Al-Chalabi <96077191+hay90der@users.noreply.github.com>
2022-08-04 02:05:35 +0300

    Update bookrnew.py

--- bookrnew.py
+++ bookrnew.py
@@ -69,66 +69,6 @@ indices = pd.Series(books.index, index=books['title'])
 
 
 
-# Function that get book recommendations based on the cosine similarity score of books tags
-def corpus_recommendations(title):
-    idx = indices[title]
-    sim_scores = list(enumerate(cosine_sim_corpus[idx]))
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:11]
-    book_indices = [i[0] for i in sim_scores]
-    
-    # create an Empty DataFrame object
-    df = pd.DataFrame(titles.iloc[book_indices])
-    df['authors'] = books.loc[books.index.values,"authors"]
-    df['similarity_score'] = [row[1] for row in sim_scores]
-    
-    return df 
-    # return titles.iloc[book_indices]
-
-# corpus_recommendations("The Hobbit")
-
-defbook = 'The Hobbit'
-bookname = st.text_input('Enter your book name',defbook)
-#bookname = input()
-
-st.table(corpus_recommendations(bookname))
-
-
-
-
-# Encryption and Decryption book name
-
-
-# Enter plain text of any byte (stream)
-enc_bookname = str(bookname)
-
-
-# A 256 bit (32 byte) key chosen
-key = "abhijit#4387926131r513f124597851"
-
-# CTR mode chosen for stream cipher
-aes = pyaes.AESModeOfOperationCTR(str.encode(key))
-
-# cipher text creation
-e = aes.encrypt(enc_bookname)
-# or, use this directly
-# e = pyaes.AESModeOfOperationCTR(str.encode(key)).encrypt(i)
-
-#print("The Encrypted Book name is" + str(e) )
-
-print("\n The Encrypted Book name is : \n", e)
-
-
-# decrypting cipher text
-# The counter mode of operation maintains state, so decryption requires a new instance be created
-aes = pyaes.AESModeOfOperationCTR(str.encode(key))
-d = aes.decrypt(e)
-# or, use this directly
-#d = pyaes.AESModeOfOperationCTR(str.encode(key)).decrypt(e)
-
-print("\n The Decrypted Book name is : \n", d)
-
-
 
 
 
