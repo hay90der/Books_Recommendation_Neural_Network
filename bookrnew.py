2742a5a807c506a6e4802e45037f01fcb451cc92
Hayder Al-Chalabi <96077191+hay90der@users.noreply.github.com>
2022-08-04 03:03:49 +0300

    Update bookrnew.py

--- bookrnew.py
+++ bookrnew.py
@@ -61,12 +61,10 @@ def corpus_recommendations(title):
 # corpus_recommendations("The Hobbit")
 
 defbook = 'The Hobbit'
-bookname = st.text_input('Enter your book name and press START Button')
+# bookname = st.text_input('Enter your book name and press START Button')
 
-if st.button('START'):
-  st.table(corpus_recommendations('The Hobbit'))
-else:
-  st.write('Nothing to show')
+st.table(corpus_recommendations('The Hobbit'))
+
 
 
 
